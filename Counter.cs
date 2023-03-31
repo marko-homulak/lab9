98f4753fdaa440f4bd6eb186e7e16b5294237609
Mark <73536533+marko-homulak@users.noreply.github.com>
2023-03-31 10:13:22 +0300

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Counter
+    {
+        private int value = 1;
+        private int digits = 1;
+        private int lower_bound_value;
+        private int upper_bound_value;
+        private bool lower_bound_set = false;
+        private bool upper_bound_set = false;
+
+        public int Value 
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public int LowerBoundValue 
+        {
+            get { return lower_bound_value; }
+            set 
+            {
+                lower_bound_value = value;
+                lower_bound_set = true;
+            }
+        }
+
+        public int UpperBoundValue
+        {
+            get { return upper_bound_value; }
+            set
+            {
+                upper_bound_value = value;
+                upper_bound_set = true;
+            }
+        }
+
+        public Counter(int start_value, int digits)
+        {
+            this.value = start_value;
+            this.digits = digits;
+        }
+
+        public string GetInDigitalFormat()
+        {
+            if (value.ToString().Length > digits)
+            {
+                string to_be_returned = "";
+
+                for (int i = 0; i < digits; i++)
+                {
+                    to_be_returned += "9";
+                }
+                if (value > 0)
+                {
+                    return to_be_returned;
+                }
+                else
+                {
+                    return "-" + to_be_returned;
+                }
+            }
+            string leftPadding = "";
+
+            for (int i = 0; i < digits - value.ToString().Length; i++)
+            {
+                leftPadding += "0";
+            }
+            return leftPadding + value.ToString();
+        }
+
+        public void Decrement()
+        {
+            if (value - 1 < lower_bound_value && lower_bound_set)
+            {
+                if (upper_bound_set)
+                {
+                    value = upper_bound_value;
+                }
+                else
+                {
+                    value = int.MaxValue;
+                }
+            }
+            else
+            {
+                value--;
+            }
+        }
+
+        public void Increment()
+        {
+            if (value + 1 > upper_bound_value && upper_bound_set)
+            {
+                if (lower_bound_set)
+                {
+                    value = lower_bound_value;
+                }
+                else
+                {
+                    value = int.MinValue;
+                }
+            }
+            else
+            {
+                value++;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private Counter counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            counter = new Counter(0, 5);
+            Counter_label.Text = counter.GetInDigitalFormat();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                counter = CreateCounterFromUserInputs();
+
+                Counter_label.Text = counter.GetInDigitalFormat();
+
+                CounterTimer.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private Counter CreateCounterFromUserInputs()
+        {
+            Counter created_counter = new Counter(int.Parse(start_value.Text), int.Parse(digits.Text));
+            if (!string.IsNullOrEmpty(lower_bound.Text))
+            {
+                created_counter.LowerBoundValue = int.Parse(lower_bound.Text);
+            }
+
+            if (!string.IsNullOrEmpty(upper_bound.Text))
+            {
+                created_counter.UpperBoundValue = int.Parse(upper_bound.Text);
+            }
+            return created_counter;
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            CounterTimer.Enabled = false;
+        }
+
+        private void increment_btn_Click(object sender, EventArgs e)
+        {
+            if (counter != null)
+            {
+                counter.Increment();
+                Counter_label.Text = counter.GetInDigitalFormat();
+            }
+        }
+
+        private void decrement_btn_Click(object sender, EventArgs e)
+        {
+            if (counter != null)
+            { 
+                counter.Decrement();
+                Counter_label.Text = counter.GetInDigitalFormat();
+            }
+        }
+
+        private void CounterTimer_Tick(object sender, EventArgs e)
+        {
+            Counter_label.Text = counter.GetInDigitalFormat();
+            counter.Increment();
+        }
+    }
+}
